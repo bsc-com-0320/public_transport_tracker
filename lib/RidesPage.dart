3d591f0a38952bafcd51c2d7c771191cc4615231
Morten Msiska <bsc-05-20@unima.ac.mw>
2025-04-22 16:56:47 +0200

    Implemented image picker

--- lib/RidesPage.dart
+++ lib/RidesPage.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class RidesPage extends StatelessWidget {
+class RidesPage extends StatefulWidget {
+  @override
+  _RidesPageState createState() => _RidesPageState();
+}
+
+class _RidesPageState extends State<RidesPage> {
+  File? _selectedImage;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,21 +54,39 @@ class RidesPage extends StatelessWidget {
               SizedBox(height: 15),
 
               // Add Photos Button
-              Container(
-                width: double.infinity,
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: Color(0xFF8B5E3B),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  children: [
-                    Icon(Icons.add, color: Colors.white, size: 40),
-                    SizedBox(height: 5),
-                    Text("Add Photos",
+              GestureDetector(
+                onTap: _pickImage,
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF8B5E3B),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      if (_selectedImage != null)
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: FileImage(_selectedImage!),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        )
+                      else
+                        Icon(Icons.add, color: Colors.white, size: 40),
+                      SizedBox(height: 5),
+                      Text(
+                        _selectedImage != null ? "Change Photo" : "Add Photos",
                         style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.bold)),
-                  ],
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               SizedBox(height: 15),
@@ -62,8 +100,8 @@ class RidesPage extends StatelessWidget {
                   context, Icons.access_time, "Departure Time", '/rides'),
               _buildClickableInputField(
                   context, Icons.people, "Capacity", '/rides'),
-              _buildClickableInputField(
-                  context, Icons.monetization_on, "Total Route Cost", '/rides'),
+              _buildClickableInputField(context, Icons.monetization_on,
+                  "Total Route Cost", '/rides'),
               _buildClickableInputField(
                   context, Icons.more_horiz, "Other", '/rides'),
 
